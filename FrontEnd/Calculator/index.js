43510bb1f3f852d8ca54ec2c812de49825d898e5
ISsam777 <119487532+ISsam777@users.noreply.github.com>
2024-07-02 19:28:35 +0100

    add folder

---
+++ FrontEnd/Calculator/index.js
@@ -0,0 +1,21 @@
+//Calculator
+
+const dispaly = document.getElementById("dispaly");
+
+function appendtoToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "Error";
+    }
+    
+}
